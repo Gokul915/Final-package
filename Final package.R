d356ec401d96f5debd4d311187b86047d26c539a
Gokul915 <kolligokulkrishna@gmail.com>
2024-12-03 06:30:36 -0500

    Add files via upload

---
+++ Final package.R
@@ -0,0 +1,10 @@
+package: Gokul
+Title: Geovisuals
+Authors: Gokul krishna kolli gokulkrishnak@usf.edu
+Description: A package to create and customize geospatial visualizations using ggplot2, tmap, and leaflet.
+Version: 0.1.0
+Depends: R3.1.2
+License: MIT + file LICENSE
+Encoding: UTF-8
+LazyData: true
+RoxygenNote: 7.2.3
